5f511067139477f19c27df93efc6f38a89a50022
alexey-ernest <alexey.ernest@gmail.com>
2016-08-29 15:21:03 +0300

    API draft

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+
+var errors = require('./lib/routes/errors');
+var routes = require('./lib/routes');
+
+var app = express();
+
+// logger
+if (app.get('env') === 'development') {
+  app.use(logger('dev'));
+} else {
+  app.use(logger());
+}
+
+// body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// static
+app.use('/', express.static(path.join(__dirname, 'build')));
+
+// routes
+app.use('/', routes);
+
+// error handlers
+app.use(errors.notfound);
+app.use(errors.error);
+
+module.exports = app;
